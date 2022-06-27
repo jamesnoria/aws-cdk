aaf8ab86997eaa19408943337f986c3447a9e982
James Noria <jnoria.dev@gmail.com>
2022-06-26 23:05:05 -0500

    gitignore correct

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import axios from "axios";
+
+export default class PersonList extends React.Component {
+  state = {
+    testing: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://bfyy8005z5.execute-api.us-east-1.amazonaws.com/prod/cv", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        const testing = res.data;
+        this.setState({ testing });
+      });
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.testing.map((test) => (
+          <img src={test.url} alt="test" />
+        ))}
+      </ul>
+    );
+  }
+}
